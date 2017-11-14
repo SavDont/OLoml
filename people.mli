ff91c0aefbe7f2c1619ebd0500f50ffa72bfb587
Samantha Dimmer <SamanthaDimmer@dhcp-uris-3619.eduroam.cornell.edu>
2017-11-14 14:33:23 -0500

    [people.mli] added specs

--- people.mli
+++ people.mli
@@ -1,15 +1,27 @@
+(* [person] is the type representing the complete data and metadata of a
+ * user of this application *)
 type person
+
+(* [personData] is the type representing incomplete data about a person
+ * that users are able to update in the SQL database *)
 type personData
+
+(* [netid] is the type representing a key used to identify users
+ * in the SQL database. *)
 type netid = string
 
+(* [get_person id] is the complete profile of the user identified by id *)
 val get_person: netid -> person
 
-(* make a sql query to get all people *)
+(* [get_all_persons ()] is the list of all profiles of users who are
+ * registered to use this application*)
 val get_all_persons: unit -> person list
 
-(* Side-effect: Updates the SQL database*)
+(* [update_person id d] updates the data corresponding to d in the
+ * SQL database that is in the profile of the user identified by id.
+ * Side-effect: updates the SQL database *)
 val update_person: netid -> personData -> person
 
-(* Side-effect: Updates the SQL database*)
+(* [add_person p] adds the p to the SQL database
+ * Side-effect: Updates the SQL database*)
 val add_person: person -> unit
-
