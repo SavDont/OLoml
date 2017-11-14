1e107ca073b65c8d68cc333bc18ddbecfdc9f42e
Samantha Dimmer <SamanthaDimmer@dhcp-uris-3619.eduroam.cornell.edu>
2017-11-14 14:30:51 -0500

    [pool.mli] added specs

--- pool.mli
+++ pool.mli
@@ -1,18 +1,32 @@
+(* [score] represents how valuable or "good" a partnership might be *)
 type score
 
-(* will be list/heap, similar in form to the pool module 
- should have a pop method - pop the head of it *)
+(* a [Pool] is an ordered representation of entries.
+ * the greatest entry is the first to be accessed, and
+ * the least entry is the last to be accessed. *)
 module type Pool = sig
+
+(* [pool] is the type representing the underlying
+ * datastructure of this module.  *)
   type 'a pool
+
+(* [empty] is the empty pool *)
   val empty    : 'a pool
+
+(* [is_empty p] is true iff p is empty. *)
   val is_empty : 'a pool -> bool
+
+(* [push i p] is p with i inserted in its correct ordered position.
+* Requires: i is not already in p *)
   val push     : 'a -> 'a pool -> 'a pool
+
+(* [peek p] is the next greatest item in p. Unlike pop,
+ * it does not remove said item. *)
   val peek     : 'a pool -> 'a
+
+(* [pop p] is p with the next greatest item removed from it. *)
   val pop      : 'a pool -> 'a pool
 end
-
-(* handle swipe logic on a single person and the logic of writing swipes 
- * to the database *)
 
 (* in the main, we will have a Pool that we continue to peek from. At every
  * iteration, we take the result of the peek and allow the user to swipe
@@ -20,23 +34,39 @@ end
  * have exhausted the pool), we can take the swipe_results (which should be
  * fully populated) and write it to the DB *)
 
-(* handle swipe logic on a single person and the logic of writing swipes 
- * to the database *)
+(* [Swipe] handles the act of deciding favorably or negatively
+ * toward items that are popped, in order, from a pool. *)
 module type Swipe = sig
-   type swipe_results
-   type decision
-   val swipe : person -> decision -> swipe_results
 
-  (* once we're done iterating through the pool, call this to write everything
-   * into the database *)
+(* [swipe_results] is a representation of the decisions
+ * a user has made regarding a person *)
+  type swipe_results
+
+(* [decision] is a representation of a user's decision regarding
+ * a person.  Said decision may be negative or positive. *)
+  type decision
+
+(* [swipe p d] is the [swipe_results] representation
+ * of the user's act of making a decision, d about a person, p. *)
+   val swipe : People.person -> decision -> swipe_results
+
+(* [write_swipe_results s] adds s to the connected database.
+ * side-effects: updates the connected database. *)
   val write_swipe_results : swipe_results -> unit
 end
 
-(* create the ordered pool based on characteristics to be determined later *)
-val poolify : People.person -> People.person list -> pool
-
-(* compatibility_score [a b] should equal compatability_score [c d] if 
+(* [compatability_score p1 p2] gives the [score] representing how
+ * "good" a partnership between p1 and p2 appears.
+ * [compatibility_score a b] should equal [compatability_score c d] if
  * a = b and c = d
- * compatibility_score [a b] should equal compatability_score [b a] *)
+ * [compatibility_score a b] should equal [compatability_score b a]
+ *)
 val compatability_score : People.person -> People.person -> score
 
+(* [poolify p p_lst] is a pool of maximum 25 people, from people_lst,
+ * who are determined to be the most compatible matches for p,
+ * based on the results of calling [compatability_score] on each
+ * person in p_lst.
+ * note: the maximum number of people + the features that determine
+ * this compatibility score are tbd. *)
+val poolify : People.person -> People.person list -> pool
