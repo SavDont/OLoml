24141af6cba8d547cbd70fd3b95507eb32045604
Samantha Dimmer <SamanthaDimmer@dhcp-uris-3619.eduroam.cornell.edu>
2017-11-14 14:30:16 -0500

    [match.mli] updated spec

--- match.mli
+++ match.mli
@@ -1,13 +1,16 @@
-(* represents the results of swipes from all students in a class *)
+(* [class_results] represents the results of swipes from all students
+ * in a class *)
 type class_results
 
-(* should query the database and generate the class_results *)
+(* [gen_class_results ()] gives the swipe results from the class of
+ * students, as queried from the SQL database. *)
 val gen_class_results : unit -> class_results
 
-(* matching of partners *)
+(* [matching] represents a system's final decision of a partner group*)
 type matching
 
-(* make the matching *)
+(* [matchify r] is a final matching of all students in a class
+ * based on r.
+ * - The specifics behind this algorithm are tbd
+ * - Handling unmatched students tbd *)
 val matchify : class_results -> matching
-
-
