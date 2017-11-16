d7bd49b1ec10d161a4e1b88d3a2bdae54d4026dc
aayushberi26 <aayush.beri26@gmail.com>
2017-11-15 21:10:52 -0500

    [professor.mli, student.mli, db.mli] updated professor and student, created db

--- professor.mli
+++ professor.mli
@@ -1,5 +1,9 @@
 type timePeriods
 
+(* [authenticate pwd] returns [true] iff pwd = the admin password in the
+ * database and [false] otherwise *)
+val authenticate : string -> bool
+
 (* [import_students dir] takes a string that represents a directory for a csv
  * and imports all the individuals into the database. Returns [true] iff the
  * function successfully reads in the directory and populates the database
@@ -13,19 +17,22 @@ val import_students: string -> bool
  * in the database. Returns [true] iff the function successfully reads the
  * time periods and writes to database. Returns [false] otherwise.
  * Example: period could be update profile period, swipe period, or match
- * period.*)
+ * period.
+ * side effect: writes to the DB *)
 val set_periods: timePeriods -> bool
 
 (* [remove_student netID] takes a student's netid and removes the associated
  * student from the database. Returns [true] if the function successfully
  * removes the student from the database. Returns [false] if the [netID] is not
- * in the database or the function fails to write to database*)
+ * in the database or the function fails to write to database
+ * side effect: writes to the DB *)
 val remove_student: Student.netid -> bool
 
 (* [add_student metaData] takes a student's meta data and adds the student to
  * the database. Returns [true] if the function successfully writes the student
  * to the database or the student is already in the database. Returns [false]
- * otherwise*)
+ * otherwise
+ * side effect: writes to the DB *)
 val add_student: Student.studentData -> bool
 
 (* [get_all_student ()] reads from the database and returns a list of students.
@@ -33,5 +40,9 @@ val add_student: Student.studentData -> bool
 val get_all_students: unit -> Student.student list
 
 (* [reset_class ()] clears the database and returns [true] iff the function
- * successfully clears the database and [false] otherwise*)
+ * successfully clears the database and [false] otherwise
+ * side effect: writes to the DB *)
 val reset_class: unit -> bool
+
+
+
--- student.mli
+++ student.mli
@@ -2,7 +2,21 @@ type student
 type studentData
 type netid = string
 
-val get_student: netid -> student
+(* [authenticate netID pwd] returns [true] iff pwd = the password associated
+ * with netID in the database and [false] otherwise *)
+val authenticate : netid -> string -> bool
 
-(* Side-effect: Updates the SQL database*)
-val update_student: netid -> studentData -> student
+(* [get_student netID] returns Some s iff a student with netID exists in the 
+ * database and s is that student. Otherwise, return None *)
+val get_student : netid -> student option
+    
+(* [update_student netID] returns Some s iff a student with netID exists
+ * in the database. s is that student updated with studentData.
+ * Otherwise, return None 
+ * side effect: writes to the DB *)
+val update_student : netid -> studentData -> student
+
+(* [get_match netID] returns Some s iff a student with netID exists in the
+ * database and that student (Ezra) has a match. s is Ezra's match and 
+ * None otherwise *)
+val get_match : netid -> student option
