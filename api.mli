0b02b8113c447e4d48d9dcdfa2c3f055be5ba1cc
aayushberi26 <aayush.beri26@gmail.com>
2017-11-23 16:21:03 -0500

    [api.mli] made request/response json format uniform

--- api.mli
+++ api.mli
@@ -8,7 +8,7 @@ val authenticate : string -> string -> bool
  *   key [netid] that specifies the netid of the student information is being
  *    requested about
  *   key [password] that specifies the password associated with netid in the 
- *    database, used to authenticate the request 
+ *    database
  *   key [type] that specifies the type of info we are trying to GET. 
  *    The value of [type] can only be student or match
  *    if [type] is student, retrieve an individual student row from database
@@ -23,21 +23,23 @@ val authenticate : string -> string -> bool
  *  [resp.status]
  *    [resp.status] should be `OK iff netid/password combination was 
  *     authenticated and valid data was retrieved from the database
-*    [resp.status] should be `Unauthorized iff netid/password did not 
+ *    [resp.status] should be `Unauthorized iff netid/password did not 
  *     authenticate
- *    [resp.status] should be `No_response iff netid/password authenticated but 
- *     no valid data was retrieved
+ *    [res.status] should be `No_response otherwise
  *  [resp.res_body]
  *    if [resp.status] is `OK
  *      [resp.res_body] should be a text json representation of the database  
  *      data in the following form:
- *      {k1 : v1, k2 : v2, ... kn : vn}
- *      where ki is a column in the database and vi is the value associated with 
- *      that column for the requested student
+ *      {netID:
+ *        {k1 : v1, k2 : v2, ... kn : vn}
+ *      }
+ *      where netID is the netid from above, ki is a column in the database and 
+ *      vi is the value associated with that column for the student with a netid
+ *      corresponding to netID
  *    if [res.status] is `Unauthorized
  *      [res.res_body] should be "Incorrect netid or password"
  *    if [res.status] is `No_response
- *      [res.res_body] should be "No valid data was retrieved. Try again later"
+ *      [res.res_body] should be "No valid response. Try again later"
  *)
 val student_get : HttpServer.request -> HttpServer.response
 
@@ -48,12 +50,15 @@ val student_get : HttpServer.request -> HttpServer.response
  *   key [netid] that specifies the netid of the student information is being
  *    requested about
  *   key [password] that specifies the password associated with netid in the 
- *    database, used to authenticate the request 
+ *    database
  * [req.req_body]
  *   [req.req_body] should be a string representing a JSON as such:
- *   {k1 : v1, k2 : v2, ... kn : vn}
- *   where ki is a column name in the database and vi is the value we want to
- *   store in that column for the specified student
+ *      {netID:
+ *        {k1 : v1, k2 : v2, ... kn : vn}
+ *      }
+ *      where netID is the netid from above, ki is a column in the database and 
+ *      vi is the value that we want to write for that column for the student
+ *      with a netid corresponding to netID
  *   if ki is an invalid column name or a column name that is not user mutable 
  *   (i.e. a metadata column), the ki : vi pair is ignored
  *   for column name ki specified in the request body where ki corresponds to a
@@ -67,28 +72,26 @@ val student_get : HttpServer.request -> HttpServer.response
  *    [res.headers] should be the default plain text Cohttp Header
  *  [res.status]
  *    [res.status] should be `OK iff netid/password combination was 
- *    authenticated and valid data was written to the database
+ *    authenticated and a valid database update was performed
  *    [res.status] should be `Unauthorized iff netid/password did not 
  *    authenticate
- *    [res.status] should be `No_response iff netid/password authenticated but
- *    data write failed or some other error occured
+ *    [res.status] should be `No_response otherwise
  *  [res.res_body]
  *    if [res.status] is `OK
  *      [res.resp_body] should be "Success"
  *    if [res.status] is `Unauthorized
  *      [res.res_body] should be "Incorrect netid or password"
  *    if [res.status] is `No_response
- *      [res.res_body] should be "No valid data was retrieved. Try again later"
+ *      [res.res_body] should be "No valid response. Try again later"
  * 
- * side effect: writes to db
+ * side effect: updates the database
  *)
 val student_post : HttpServer.request -> HttpServer.response
 
 (* [admin_get req] is the callback that handles all admin level GET requests.
  * requires: 
  * [req.headers]
- *   key [password] that specifies the admin password, used to authenticate the
- *   request
+ *   key [password] that specifies the admin password
  *   key [type] that specifies the type of info we are trying to GET. 
  *     [type] can be student_indiv, match_indiv, swipe_indiv, student_all, 
  *     swipe_all, or match_all
@@ -100,7 +103,8 @@ val student_post : HttpServer.request -> HttpServer.response
  *     if [type] is student_all, retrieve all students from the database
  *     if [type] is swipe_all, retrieve the swipe results from all students
  *     in the database
- *     if [type] is match_all, retrieve the match for all students
+ *     if [type] is match_all, retrieve the match for all students from the
+ *     database
  *   key [netid] that specifies the netid of the student we are trying to GET
  *     data about
  *     only needed iff [type] is student_indiv, match_indiv, or swipe_indiv
@@ -115,8 +119,7 @@ val student_post : HttpServer.request -> HttpServer.response
  *    [res.status] should be `OK iff the admin password was authenticated
  *     and valid data was retrieved from the database
  *    [res.status] should be `Unauthorized iff password did not authenticate
- *    [res.status] should be `No_response iff password authenticated but no
- *     valid data was retrieved or some other error occured
+ *    [res.status] should be `No_response otherwise
  *  [res.res_body]
  *    if [res.status] is `OK
  *      [res.res_body] should be a text json representation 
@@ -130,23 +133,23 @@ val student_post : HttpServer.request -> HttpServer.response
  *         {k1 : snv1, k2 : snv2, ... kn : snvn}
  *       }
  *      where si is a netid, kj is a column in the database and sivj is the
- *      value associated with column kj for student si in the database
+ *      value associated with column kj for student with netid si in the 
+ *      database
  *    if [res.status] is `Unauthorized, 
  *      [res.res_body] should be "Incorrect password"
  *    if [res.status] is `No_response
- *      [res.res_body] should be "No valid response retrieved. Try again later"
+ *      [res.res_body] should be "No valid response. Try again later"
  *)
 val admin_get : HttpServer.request -> HttpServer.response
 
 (* [admin_post req] is the callback that handles all admin level POST requests.
  * requires: 
  * [req.headers]
- *  key [password] that specifies the admin password, used to authenticate the
- *  request
+ *  key [password] that specifies the admin password
  *  key [type] that specifies the type of action we are trying to take. 
  *   key [type] can either be add or remove
  *   if [type] is add, add any number of students to the database
- *   if [type] is remove, remove any number of student to the database
+ *   if [type] is remove, remove any number of students from the database
  * [req.req_body] 
  *  [req.req_body] should be a string representing a JSON
  *  if [type] is add, the body should be as follows:
@@ -170,6 +173,7 @@ val admin_get : HttpServer.request -> HttpServer.response
  *   si, hence the wildcard.
  *   if a student with netid si is not in the database, ignore the si : _ pair
  *   if a student with netid si is in the database, delete the entire row
+ *   associated with that student
  * [req.params] - ignored 
  * 
  * returns:
@@ -178,18 +182,17 @@ val admin_get : HttpServer.request -> HttpServer.response
  *    [res.headers] should be the default plain text Cohttp Header
  *  [res.status]
  *    [res.status] should be `OK iff the admin password was authenticated
- *     and valid data was retrieved from the database
+ *     and a valid database update was performed
  *    [res.status] should be `Unauthorized iff password did not authenticate
- *    [res.status] should be `No_response iff password authenticated but
- *     data write failed or some other error occured
+ *    [res.status] should be `No_response otherwise
  *  [res.res_body]
  *    if [res.status] is `OK
  *      [res.resp_body] should be "Success"
  *    if [res.status] is `Unauthorized
  *      [res.res_body] should be "Incorrect password"
  *    if [res.status] is `No_response
- *      [res.res_body] should be "No valid response retrieved. Try again later"
+ *      [res.res_body] should be "No valid response. Try again later"
  * 
- * side effect : writes to db
+ * side effect : updates the database
  *)
 val admin_post : HttpServer.request -> HttpServer.response
