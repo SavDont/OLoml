675476828be8157b2a73a781159f23e45f4d0a13
aayushberi26 <aayush.beri26@gmail.com>
2017-11-29 05:36:17 -0500

    [oclient.ml] some small doc updates

--- oclient.ml
+++ oclient.ml
@@ -25,15 +25,18 @@ let handle_resp = fun (resp, body) ->
   body |> Cohttp_lwt.Body.to_string >|= fun body -> ();
   (resp |> Response.status, body)
 
+(* helper to make get requests *)
 let get_req ?ctx ?headers uri = 
   let lwt_resp = Client.get ?ctx ?headers uri >>= handle_resp in
   Lwt_main.run lwt_resp
 
+(* helper to make post requests *)
 let post_req ?ctx ?body ?chunked ?headers uri = 
   let lwt_resp =
     Client.post ?ctx ?body ?chunked ?headers uri >>= handle_resp in
   Lwt_main.run lwt_resp
 
+(* helper to make delete requests *)
 let delete_req ?ctx ?body ?chunked ?headers uri = 
   let lwt_resp =
     Client.delete ?ctx ?body ?chunked ?headers uri >>= handle_resp in
