9355fe1ae34f5f6b6117412128492398e626bf3f
Myra Gupta <mg954@cornell.edu>
2017-12-02 00:06:31 -0500

    [db.ml/mli] took our dbh parameter

--- db.ml
+++ db.ml
@@ -8,19 +8,20 @@ let stu_tbl = "Students"
 let match_tbl = "Matches"
 let creds_tbl = "Credentials"
 let periods_tbl = "Periods"
+let dbh = PGOCaml.connect ()
 
-let check_cred_query dbh netid pwd =
+let check_cred_query netid pwd =
   match (PGSQL(dbh) "SELECT Password FROM $creds_tbl WHERE Netid = $netid") with
   | i -> if i = pwd then true else false
   | _ -> false
 
-let check_period_set dbh =
+let check_period_set =
   if ((PGSQL(dbh) "SELECT Update FROM $periods_tbl") != None
       ||  (PGSQL(dbh) "SELECT Swipe FROM $periods_tbl") != None
       ||  (PGSQL(dbh) "SELECT Match FROM $periods_tbl") != None) then true
   else false
 
-let set_period_query dbh periods =
+let set_period_query periods =
   let jsn = from_string periods in
   let update_dt = jsn |> Util.member "update" |> Util.to_float_option in
   let swipe_dt = jsn |> Util.member "swipe" |> Util.to_float_option in
@@ -36,7 +37,7 @@ let set_period_query dbh periods =
         VALUES ($update_dt, $swipe_dt, $match_dt)"
   else ()
 
-let get_period_query dbh =
+let get_period_query =
   match PGSQL (dbh) "SELECT Update, Swipe, Match FROM $periods_tbl" with
   |(jupdate, jmatch, jswipe) ->
     let upd = ("update", jupdate) in
@@ -44,7 +45,7 @@ let get_period_query dbh =
     let swi = ("swipe", jswipe) in
   let jsonobj = `Assoc[upd;mat;swi] in Yojson.Basic.to_string jsonobj
 
-let get_student_query dbh netid =
+let get_student_query netid =
   match PGSQL(dbh) "SELECT * FROM $stu_tbl WHERE Netid = $netid" with
   |(jnetid,jname,jyr,jsched,jcourses,jhrs,jprof,jloc) ->
     let name = ("name", jname) in
@@ -58,13 +59,36 @@ let get_student_query dbh netid =
   let jsonobj = `Assoc[name;netid;year;sched;courses;hrs;prof;loc] in
   Yojson.Basic.to_string jsonobj
 
-let get_stu_match_query dbh netid =
+let get_stu_match_query netid =
   let partner = PGSQL(dbh) "SELECT Match FROM $match_tbl WHERE Stu1 = $netid" in
-  get_student_query dbh partner
+  match partner with
+  |Some i ->
+    if i = "UNMATCHED" then
+      let name = ("name", None) in
+      let netid = ("netid", Some "UNMATCHED") in
+      let year = ("year", None) in
+      let sched = ("schedule", None) in
+      let courses = ("courses_taken", None) in
+      let hrs = ("hours_to_spend", None) in
+      let prof = ("profile_text", None) in
+      let loc = ("location", None)) in
+      let jsonobj = `Assoc[name;netid;year;sched;courses;hrs;prof;loc] in
+      Yojson.Basic.to_string jsonobj
+    else get_student_query dbh i
+  |_ -> let name = ("name", None) in
+    let netid = ("netid", None) in
+    let year = ("year", None) in
+    let sched = ("schedule", None) in
+    let courses = ("courses_taken", None) in
+    let hrs = ("hours_to_spend", None) in
+    let prof = ("profile_text", None) in
+    let loc = ("location", None)) in
+    let jsonobj = `Assoc[name;netid;year;sched;courses;hrs;prof;loc] in
+    Yojson.Basic.to_string jsonobj
 
 (*helper functions that change each specific field if the field exists in the
  * json *)
-let change_sched dbh net info =
+let change_sched net info =
   let jsn = from_string info in
   begin match jsn |> Util.member "schedule" |> Util.to_string_option with
     |None -> ()
@@ -72,35 +96,35 @@ let change_sched dbh net info =
                 WHERE Netid = $net"
   end
 
-  let change_courses dbh net info =
+  let change_courses net info =
     let jsn = from_string info in
     begin match jsn |> Util.member "classes_taken" |> Util.to_string_option with
       |None -> ()
       |i -> PGSQL(dbh) "UPDATE $stu_tbl SET Courses = $i WHERE Netid = $net"
     end
 
-  let change_hours dbh net info =
+  let change_hours net info =
     let jsn = from_string info in
     begin match jsn |> Util.member "hours_to_spend" |> Util.to_int_option with
       |None -> ()
       |i -> PGSQL(dbh) "UPDATE $stu_tbl SET Hours = $i WHERE Netid = $net"
     end
 
-  let change_prof dbh net info =
+  let change_prof net info =
     let jsn = from_string info in
     begin match jsn |> Util.member "profile_text" |> Util.to_string_option with
       |None -> ()
       |i -> PGSQL(dbh) "UPDATE $stu_tbl SET Profile = $i WHERE Netid = $net"
     end
 
-  let change_loc dbh net info =
+  let change_loc net info =
     let jsn = from_string info in
     begin match jsn |> Util.member "location" |> Util.to_string_option with
       |None -> ()
       |i -> PGSQL(dbh) "UPDATE $stu_tbl SET Location = $i WHERE Netid = $net"
     end
 
-let change_stu_query dbh net info =
+let change_stu_query net info =
   let a = change_sched dbh net info in
   let b = change_courses dbh net info in
   let c = change_hours dbh net info in
@@ -109,7 +133,7 @@ let change_stu_query dbh net info =
   match [a;b;c;d;e] with
   |_ -> ()
 
-let admin_change_query dbh info =
+let admin_change_query info =
   let jsn = from_string info in
   let new_name = jsn |> Util.member "name" |> Util.to_string_option in
   let new_id = jsn |> Util.member "netid" |> Util.to_string_option in
@@ -118,5 +142,5 @@ let admin_change_query dbh info =
     ($new_id, $new_name, $new_year) ON DUPLICATE KEY UPDATE
     Name = $new_name, Year = $new_year"
 
-let reset_class dbh =
+let reset_class =
   PGSQL(dbh) "TRUNCATE $stu_tbl, $match_tbl, $creds_tbl, $periods_tbl"
