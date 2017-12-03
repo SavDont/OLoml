4ac43254ace33782bb294f6f40933e3911e0c11c
aayushberi26 <aayush.beri26@gmail.com>
2017-12-03 12:02:24 -0500

    [api.*, server] removed admin get

--- server.ml
+++ server.ml
@@ -12,7 +12,6 @@ let _ =
   HttpServer.add_route (`POST, "/matches/") Api.matches_post;
   HttpServer.add_route (`GET, "/student/") Api.student_get;
   HttpServer.add_route (`POST, "/student/") Api.student_post;
-  HttpServer.add_route (`GET, "/admin/") Api.admin_get;
   HttpServer.add_route (`POST, "/admin/") Api.admin_post;
   HttpServer.add_route (`DELETE, "/admin/") Api.admin_delete;
   HttpServer.run ~port:8000 ()
