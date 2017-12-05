8cb1e7ee9c21c0fe0014721f2a566191d5324a24
sd549 <sd549@cornell.edu>
2017-12-05 16:21:10 -0500

    [server.ml] fixed server roude for admin post

--- server.ml
+++ server.ml
@@ -13,6 +13,6 @@ let _ =
   HttpServer.add_route (`GET, "/student/") Api.student_get;
   HttpServer.add_route (`POST, "/student/") Api.student_post;
   HttpServer.add_route (`GET, "/admin/") Api.admin_get;
-  HttpServer.add_route (`GET, "/student/") Api.student_get;
+  HttpServer.add_route (`POST, "/admin/") Api.admin_post;
   HttpServer.add_route (`DELETE, "/admin/") Api.admin_delete;
   HttpServer.run ~port:8000 ()
